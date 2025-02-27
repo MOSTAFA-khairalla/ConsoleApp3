f178febed7fa245b02a4779863e7d6710ef38e1e
Mostafa <mostafakhairalla7899@gmail.com>
2025-02-27 15:43:36 +0200

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        int sum = 0;
+        for (int i = 1; i < number; i++)
+        {
+            sum += i;
+        }
+
+        Console.WriteLine($"Sum of numbers from 1 to {number} is: {sum}");
+    }
+}
